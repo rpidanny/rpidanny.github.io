f56ca4be4cb28e6908283d854fae190f1e626b0e
rpidanny <abhishekmaharjan1993@gmail.com>
2016-08-13 22:17:02 +0545

    added progressive web

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+var cacheName = 'test-2';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/thinking.png',
+  '/manifest.json',
+  '/fancyBox/lib/jquery-1.10.1.min.js',
+  '/fancyBox/source/jquery.fancybox.js?v=2.1.5',
+  '/scripts/fancyBoxScript.js',
+  '/scripts/vendor.js',
+  '/scripts/plugins.js',
+  '/scripts/main.js',
+  '/images/to-top@2x.png',
+  '/images/vcard.png',
+  '/images/abhishek1.jpg',
+  '/fancyBox/source/jquery.fancybox.css?v=2.1.5',
+  '/styles/main.css',
+  '/styles/vendor.css',
+  'http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css',
+
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        console.log('[ServiceWorker] Removing old cache', key);
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
